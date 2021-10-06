6bf89612b2a47cb12766a2ba9806c1433d1d9ec9
l1t-w1n <lmv06013@gmail.com>
2021-10-06 11:58:26 +0200

    ex57 qui marche

--- Exo57.py
+++ Exo57.py
@@ -3,9 +3,9 @@ def preminfn(n):
     l=[True for i in range(n+1)]
     l[0]=False
     l[1]=False
-    for n in range(1,len(l)):
-        for i in range(1,int(m.sqrt(n))):
+    for n in range(2,len(l)):
+        for i in range(2,n):
             if n%i==0:
                 l[n]=False
     return l
-print(preminfn(20))
+print(preminfn(15))
