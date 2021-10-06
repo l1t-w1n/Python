94eede6482cbac5d3a1af1f793067cb490f180a4
l1t-w1n <lmv06013@gmail.com>
2021-10-06 12:16:27 +0200

    ex57 qui marche avec racine de n

--- Exo57.py
+++ Exo57.py
@@ -2,10 +2,11 @@ import math as m
 def preminfn(n):
     l=[True for i in range(n+1)]
     l[0]=False
-    l[1]=False
+    l[1]=False    
     for n in range(2,len(l)):
-        for i in range(2,n):
-            if n%i==0:
-                l[n]=False
+        d=2
+        while d<=m.sqrt(n) and n%d!=0:
+            d+=1
+        l[n]=d*d>n
     return l
-print(preminfn(15))
+print(preminfn(11))
