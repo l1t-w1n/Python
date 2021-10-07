4b8f34f6fa79fe16dfd3a9cd1e941de36cd0c751
l1t-w1n <lmv06013@gmail.com>
2021-10-07 11:02:24 +0200

    Amelioration de recherche dichotomique et petits ameliorations de code

--- Exercices.py
+++ Exercices.py
@@ -1,12 +1,15 @@
+from math import *
+from turtle import *
+import random as r
+
 ##Exercice 4##
-import math as m
 a=5
 b=4
 c=-1
 equation = f"{a}*X**2 + {b}*X + {c} = 0"
 d= b**2 - 4*a*c
-x1= ((-b)+m.sqrt(d))/(2*a)
-x2= ((-b)-m.sqrt(d))/(2*a)
+x1= ((-b)+sqrt(d))/(2*a)
+x2= ((-b)-sqrt(d))/(2*a)
 print(f"L'équation {equation} admet comme solutions x1={x1} et x2={x2} ")
 
 ##Exercice 5##
@@ -15,26 +18,19 @@ E= 100*0.10**n
 print (f"L'épargne au bout de {n} ans est de {E} ")
 
 ##Exercice 6##
-
-"""
 a=9
 b=11
 c=a
 a=b
 b=c
-"""
-
 
 ##Exercice 7##
-"""
-
 print(EquationDuSecondDegre(a=int(input("valeur de a ? ")),\
 b=int(input("valeur de b ? ")),\
 c=float(input("valeur de c ? "))))
 
-"""
-##Exercice 8##
 
+##Exercice 8##
 def calculatrice(a,b,ope):
     if ope == "+":
         return a+b
@@ -62,7 +58,6 @@ for i in range(0,11):
     print(2**i)
 
 ## Exercice 10
-
 n=int(input("n ?"))
 somme=0
 for i in range(0,n+1):
@@ -70,7 +65,6 @@ for i in range(0,n+1):
 print(somme)
 
 ##Exercice 11##
-
 n=int(input("n impair ?"))
 assert n%2==1, 'bah non'
 produit=1
@@ -79,13 +73,11 @@ for i in range(1,n+1,2):
 print(produit)
 
 ##Exercice 12##
-
 for i in range(1,11):
     for j in range(1,11):
         print(i*j)
 
 ##Exercice 13##
-
 n=int(input("n?"))
 for i in range(0,n):
     for j in range(0,n-i-1):
@@ -94,10 +86,7 @@ for i in range(0,n):
         print("*",end=" ")
     print()
 
-
-
 ##Exercice 14##
-from turtle import *
 color("blue")
 forward(100)
 left(72)
@@ -111,28 +100,20 @@ forward(100)
 left(72)
 exitonclick()
 
-from turtle import *
-from math import *
-
 ##Exercice 15##
-
 def Polygone(long,n):
     for i in range(0,n):
         forward(long/n)
         left(360/n)
-#print(Polygone(int(input("la longueur ?")),int(input("le nombre de côté ?"))))
 
 ##Exercice 16##
-
 def Spirale(n):
     color('green')
     for i in range(0,n):
         forward(2+i/4)
         left(30-i/12)
-#print(Spirale(240))
 
 ##Exercice 17##
-
 def graph_fonc():
     setup(1000,1000)
     up()
@@ -140,32 +121,27 @@ def graph_fonc():
         y=(1/100)*(x**2)
         goto(x,y)
         down()
-#print(graph_fonc())
 
 ##Exercice 18##
-
 def Cercle(n):
    up() 
-   for i in range(0,n-1):
-       
+   for i in range(0,n-1):       
         xi=200*cos((2*i*3.14)/n)
         yi=200*sin((2*i*3.14)/n)
         goto(xi,yi)
         down()
-#print(Cercle(100))
 
 ##Exercice 19##
 
 ##Exercice 20##
-import math as m
 a=5
 b=4
 c=-1
 equation = f"{a}*X**2 + {b}*X + {c} = 0"
 d= b**2 - 4*a*c
 if d > 0 :
-    x1= ((-b)+m.sqrt(d))/(2*a)
-    x2= ((-b)-m.sqrt(d))/(2*a)
+    x1= ((-b)+sqrt(d))/(2*a)
+    x2= ((-b)-sqrt(d))/(2*a)
     print(f"L'équation {equation} admet deux solutions x1={x1} et x2={x2} ")
 elif d == 0 :
     x1=-b/(2*a)
@@ -174,7 +150,6 @@ else :
     print(f"L'équation n'a pas de solution appartenant à R")
 
 ##Exercice 21##
-import random as r
 a=r.randrange(1,13)
 b=r.randrange(1,13)
 q=int(input(f"Combien vaut le produit de {a} et {b} ?")) 
@@ -192,7 +167,6 @@ for c in range(10) :
                 print(n)
 
 ##Exercice 23##
-import math as m
 print("Choisissez trois nombre a, b et c tel que a<=b<=c.")
 a=int(input("Entrez a:\n"))
 b=int(input("Entrez b:\n"))
@@ -207,43 +181,37 @@ if a<=b<=c and (a+b)>c :
     else :
         print("le triangle est quelconque")
 
-if m.cos(a) + m.cos(b) + m.cos(c) >= 0:
+if cos(a) + cos(b) + cos(c) >= 0:
    print("angle aigu")
 else:
    print("angle obtus")
 
 ##Exercice 24##
-
-import random as ra
-a=ra.randint(0,99)
-5
+a=r.randint(0,99)
 for n in range(6):
-    r=int(input("enter r"))
-    if r==a:
+    R=int(input("enter r"))
+    if R==a:
         print("c'est correcte")
         break
-    elif r>a:
+    elif R>a:
         print("r est trop grand")
     else:
         print("r est trop petit")
 
 ##Exercice 25##
-import random as ra
-a=ra.randint(0,99)
-5
+a=r.randint(0,99)
 for n in range(6):
     a+=ra.randint(-3,3)
-    r=int(input("enter r"))
-    if r==a:
+    R=int(input("enter r"))
+    if R==a:
         print("c'est correcte")
         break
-    elif r>a:
+    elif R>a:
         print("r est trop grand")
     else:
         print("r est trop petit")
 
 ##Exercice 26##
-
 def affiche_table_de_7(a):
     for n in range(11):
         print(f"{a}*{n}={7*n}")
@@ -257,15 +225,11 @@ def affiche_bonjour() :
 def affiche_table(n):
     for i in range(11):
         print(f"{n}*{i}={n*i}")
-    
-#print(affiche_table(4))
 
 ##Exercice 29##
 def affiche_salutation(formule):
     p=input("Quel est votre prénom ? \n")
     print (f"{formule} {p}")
-
-#print(affiche_salutation("Coucou"))
 
 ##Exercice 30##
 def demande_prenom_nom():
@@ -287,7 +251,6 @@ def equation_second_deg(a,b,c) :
     else :
         print(f"L'équation n'a pas de solution appartenant à R")
 
-
 ##Exercice 32 ## 
 def multiplication(a,b) :
     n=a
@@ -302,10 +265,8 @@ def fact(n):
         return 1
     else :
         return n*fact(n-1)
-#print(fact(5))
 
 ##Exercice 34##
-
 def IsPrime(n) :
     d = 2
     while n%d != 0 :
@@ -313,7 +274,6 @@ def IsPrime(n) :
     return d==n
 
 ##Exercice 35##
-
 def IsPrime2(n) :
     d = 2
     while d*d <= n and n%d != 0 :
@@ -321,7 +281,6 @@ def IsPrime2(n) :
     return d*d > n
 
 ##Exercice 36##
-
 def IsPrime3(n) :
     if n %2 == 0 :
         return n==2
@@ -331,7 +290,6 @@ def IsPrime3(n) :
     return d*d > n
 
 ##Exercice 37##
-
 def ListPrime(n):
     rep=[]
     for i in range(1,n):
@@ -339,13 +297,8 @@ def ListPrime(n):
             rep.append(i)
     return rep
 
-
-#print(ListPrime(5))
-
-
 ##Exercice 38##
-from turtle import *
-def Polygone(long,n):
+def Polygone2(long,n):
     def main_polygone() :
         forward(long/n)
         left(360/n) 
@@ -359,11 +312,9 @@ def Polygone(long,n):
                 main_polygone()
             else :
                 color('blue')
-                main_polygone()          
-#print(Polygone(int(input("la longueur ?\n")),int(input("le nombre de côté ?\n"))))
+                main_polygone()
 
 ##Exercice 41##
-
 def pgcd(a,b) :
     while a!=b :
         if a>b :
@@ -373,22 +324,18 @@ def pgcd(a,b) :
     return a 
 
 ##Exercice 42##
-def moyenne() :
+def moyenne():
     a=b=n=m=0
-
-    while a >= 0 :
+    while a >= 0:
         a = int(input(f"Entrez un nombre :\n"))
         b+=a
         n+=1
-        if b==a :
+        if b==a:
             print(b)
         else :  
             m=b/n
             print(m)
-#print(moyenne())
 
-
-import math as m
 ###Exercice 43###
 def ppd(n):
     for d in range(2,n+2):
@@ -637,11 +584,9 @@ def recherche(l,x):
 def rechdic(l, x):
     mid=int(len(l)/2)
     if x==l[mid]:
-    	return True
-    elif len(l)==1 and l[0]==x:
-    	return True
-    elif len(l)==1 and l[0]!=x:
-    	return False
+    	return True 
+    elif len(l)==1:
+    	return l[0]==x
     elif x>l[mid]:
     	return rechdic(l[mid:],x)
     else:
@@ -767,12 +712,11 @@ def moins(n,x):
     return n<x
 def sup3(n,x):
     return n>x
-def split(l):
+def split2(l):
     l1=list(filter(lambda seq: moins(seq, l[0]),l))
     l2=[l[0]]
     l3=list(filter(lambda seq: sup(seq, l[0]),l))
     return l1+l2+l3
-
 
 ###Exercice 90###
 ventes={"Vincent":14, "Mickael":19, "Michel":15, "Annie":21}
